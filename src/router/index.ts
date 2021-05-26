a073fa666ce6af3b1dbcf52544a51e2ab89ec47a
李林林 <leeleanlean@163.com>
2021-05-26 19:54:01 +0800

    router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
-import Home from '../views/Home.vue'
+
+import dashboard from './dashboard'
 
 Vue.use(VueRouter)
 
@@ -8,16 +9,9 @@ const routes: Array<RouteConfig> = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: () => import('@/views/Home/index.vue')
   },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  dashboard
 ]
 
 const router = new VueRouter({
---
+++ src/views/Dashboard/Index/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <section class="dashboard-index">
+    dashboard-index
+  </section>
+</template>
+<script>
+export default {
+  name: 'DashboardIndex',
+  components: {
+  }
+}
+</script>
